6513457161660ab73ef09efd1bf9f0746b80f598
jpvardhan <jpvardhan.3@gmail.com>
2025-04-29 02:24:29 -0400

    Initial commit with basic Node.js application

--- custom-nodejs-lambda-app/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello from Custom Build Environment!' });
-});
-
-module.exports = app;
--- custom-nodejs-lambda-app/test/test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app');
-
-test('Hello endpoint returns correct message', async () => {
-  const res = await request(app).get('/');
-  
-  expect(res.statusCode).toBe(200);
-  expect(res.body).toEqual({ message: 'Hello from Custom Build Environment!' });
-});
